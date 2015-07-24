7c56e4deb338616ae03be41227a883befa4f6a8e
Taylor0101 <rs1900163@163.com>
2015-07-24 10:37:12 +0800

    Initial Commit

---
+++ AFNnetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNnetworking
+//
+//  Created by RSTaylor on 15/7/24.
+//  Copyright (c) 2015年 RSTaylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNnetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNnetworking
+//
+//  Created by RSTaylor on 15/7/24.
+//  Copyright (c) 2015年 RSTaylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
